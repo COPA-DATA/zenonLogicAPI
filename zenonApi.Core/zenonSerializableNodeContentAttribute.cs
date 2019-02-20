5bf9624ec491fbe3e9b1b2e1cc8d2bd79e843fc5
Mathias Lackner <hello@wobuntu.at>
2019-02-20 17:07:31 +0100

    Added some documentation and a few further classes

---
+++ zenonApi.Core/zenonSerializableNodeContentAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zenonApi.Core
+{
+  [AttributeUsage(AttributeTargets.Property)]
+  public class zenonSerializableNodeContentAttribute : Attribute
+  {
+    public zenonSerializableNodeContentAttribute() { }
+  }
+}
