f9daadaefd170452949523ceea478ca17fa12bcc
VirusRC <thomas.mike@gmx.at>
2019-02-20 17:06:43 +0100

    Add modules variables

---
+++ zenonApi.Logic/LogicGlobalVariables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using zenonApi.Core;
+
+namespace zenonApi.Logic
+{
+  public class LogicGlobalVariables : zenonSerializable<LogicGlobalVariables, LogicProject, LogicProject>
+  {
+    #region zenonSerializable Implementation
+
+    public override LogicProject Parent { get; protected set; }
+    public override LogicProject Root { get; protected set; }
+
+    protected override string NodeName => "variables";
+
+    #endregion  
+
+    [zenonSerializableNode("vargroup")]
+    public List<LogicVariableGroup> VariableGroups { get; set; }
+
+
+  }
+}
