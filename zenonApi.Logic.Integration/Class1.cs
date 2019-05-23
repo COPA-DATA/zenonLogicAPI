54908fa0aaa756016bb753205594de5ef4fafc05
VirusRC <thomas.mike@gmx.at>
2019-05-23 11:43:32 +0200

    Initial check in of logic integration csproj

---
+++ zenonApi.Logic.Integration/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zenonApi.Logic.Integration
+{
+    public class Class1
+    {
+    }
+}
