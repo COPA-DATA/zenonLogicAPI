5ed020f55dc4dc8a1351edae41f4b6f4aadca201
Mathias Lackner <Mathias.Lackner@copadata.com>
2020-07-23 14:10:36 +0200

    Stopping work on this, commiting anyways so others may continue

--- zenonApi.Core.Tests/Nullables.Test.cs
+++ zenonApi.Core.Tests/Nullables.Test.cs
@@ -1,7 +1,44 @@
-﻿namespace zenonApi.Core.Tests
+﻿using zenonApi.Serialization;
+
+namespace zenonApi.Core.Tests
 {
   public class Nullables
   {
-    // TODO: Check for both, attributes and nodes, with serialization and deserialization
+    private class NullablePrimitives
+    {
+      [zenonSerializableNode("NodeNotNull")]
+      public int? NodeNotNull { get; set; }
+
+      [zenonSerializableNode("NodeNull")]
+      public int? NodeNull { get; set; }
+
+      [zenonSerializableNode("AttributeNotNull")]
+      public int? AttributeNotNull { get; set; }
+
+      [zenonSerializableNode("AttributeNull")]
+      public int? AttributeNull { get; set; }
+    }
+
+    private enum TestEnum
+    {
+      ValueA,
+      ValueB,
+      ValueC
+    }
+
+    private class NullableEnums
+    {
+      
+    }
+
+    private class NullableItemsInList
+    {
+
+    }
+
+    private class NullableItemsInNestedList
+    {
+
+    }
   }
 }
